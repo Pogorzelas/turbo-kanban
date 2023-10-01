5fb7e912df5f5b2dd494b1a61bdc8e83abafacf5
Tomek <t.pogorzelski@telemedi.com>
2023-10-01 17:17:04 +0200

    feat: add styles to button

--- packages/ui/src/atoms/button/button.component.tsx
+++ packages/ui/src/atoms/button/button.component.tsx
@@ -1,11 +1,26 @@
+import { Button as BaseButton } from '@mui/base/Button';
+import clsx from 'clsx';
 import type { ButtonProps } from './button.types';
 
-function Button ({ children }: ButtonProps) {
+function Button ({
+  children,
+  className,
+  isDisabled,
+  type = 'button',
+}: ButtonProps) {
   return (
-    <div>
-      <h1>button</h1>
+    <BaseButton
+      className={
+        clsx(
+          'cursor-pointer disabled:cursor-not-allowed text-sm font-sans bg-violet-500 hover:bg-violet-600 active:bg-violet-700 text-white rounded-lg font-semibold px-4 py-2 border-none disabled:opacity-50',
+          className,
+        )
+      }
+      disabled={isDisabled}
+      type={type}
+    >
       {children}
-    </div>
+    </BaseButton>
   );
 }
 
--- packages/ui/src/atoms/button/button.types.ts
+++ packages/ui/src/atoms/button/button.types.ts
@@ -1,7 +1,12 @@
 import type { ReactNode } from 'react';
 
+type ButtonTypes = 'button' | 'submit' | 'reset';
+
 type ButtonProps = {
   children?: ReactNode;
+  className?: string;
+  type?: ButtonTypes;
+  isDisabled?: boolean;
 }
 
-export type { ButtonProps }
+export type { ButtonProps, ButtonTypes }
